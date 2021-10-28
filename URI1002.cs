3ee877eb7d4d610ee985e31759cf61569c266d0e
D4n1l0sbg <danilosbg50@gmail.com>
2021-10-28 20:12:53 -0300

    URI1002
    Área do Circulo.

---
+++ URI1002.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+
+                double n = 3.14159;
+                double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                double a = Math.Pow(raio, 2.0);
+                double area = a * n;
+                Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
