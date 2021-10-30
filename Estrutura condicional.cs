3462cc5d957b3843a4b23ffa91044da167fb303f
D4n1l0sbg <danilosbg50@gmail.com>
2021-10-30 15:42:10 -0300

    Create Estrutura condicional.cs
    Exercício sobre estrutura condicional.

---
+++ Estrutura condicional.cs
@@ -0,0 +1,33 @@
+﻿ using System;
+
+namespace menor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            String[] vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            c = int.Parse(vet[2]);
+
+            if (a < b && a < c) {
+            
+                Console.WriteLine("MENOR = " + a);
+            }
+            else if (b < a && b < c) {
+
+                Console.WriteLine("MENOR = " + b);
+            }
+            
+            else if (c < a && c < b) {
+
+                Console.WriteLine("MENOR = " + c);
+            }
+
+                            
+        }
+    }
+}
