dbdce0cde6febcaa5e3e293da9fe4787f95d5b09
D4n1l0sbg <danilosbg50@gmail.com>
2021-10-31 20:24:36 -0300

    Create URI1038.cs

---
+++ URI1038.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI1038
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cod, qtd;
+            double total;
+
+            String[] vet = Console.ReadLine().Split(' ');
+
+            cod = int.Parse(vet[0]);
+            qtd = int.Parse(vet[1]);
+
+
+            switch (cod)
+            {
+                case 1:
+                    total = qtd * 4.00;
+                    Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 2:
+                    total = qtd * 4.50;
+                    Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 3:
+                    total = qtd * 5.00;
+                    Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 4:
+                    total = qtd * 2.00;
+                    Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 5:
+                    total = qtd * 1.50;
+                    Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+            }
+                
+
+
+        }
+    }
+}
