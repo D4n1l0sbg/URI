e31ab393a236740e9cac4621893ed7f3ecbf0af3
D4n1l0sbg <danilosbg50@gmail.com>
2021-11-02 16:55:29 -0300

    Create URI1048.cs

---
+++ URI1048.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI1048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, reajuste, novoSalario, taxa;
+
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (salario <= 400.0)
+            {
+                taxa = 15.0;
+            }
+            else if (salario <= 800.0)
+            {
+                taxa = 12.0;
+            }
+            else if (salario <= 1200.0)
+            {
+                taxa = 10.0;
+            }
+            else if (salario <= 2000.0)
+            {
+                taxa = 7.0;
+            }
+            else
+            {
+                taxa = 4.0;
+            }
+
+            reajuste = salario * taxa / 100;
+            novoSalario = salario + reajuste;
+
+            Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste Ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Novo salario: " + taxa.ToString("F0", CultureInfo.InvariantCulture) + " %");
+        }
+    }
+}
